2713cf834718a4fe7dcc50150ac75c6aaab8cd99
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2022-10-30 16:33:59 -0700

    added dark mode button and js
    Co-authored by: Noah Cowan <nsncowan@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+  let darkMode = document.querySelector(".dark-mode");
+  let lightMode = document.querySelector(".light-mode");
+  let body = document.body;
+
+  darkMode.onclick = function() {
+    body.style.backgroundColor = "black";
+    body.style.color = "white";
+  };
+
+
+
+
+
+};
